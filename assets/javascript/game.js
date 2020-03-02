109f0cc6f2cadcaa583010635f247dd44f91a1d3
Yuko Uda <yuko.uda0110@gmail.com>
2020-03-01 22:25:33 -0800

    Create instance of Character class for each character, and also make createCharPanels function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,9 @@
 class Character {
   constructor(name, healthPoints, attackPower, counterAttackPower, image) {
     this.name = name;
-    this.healthPoints = healthPoints;
-    this.attackPower = attackPower;
-    this.counterAttackPower = counterAttackPower;
+    this.healthPoints = parseInt(healthPoints);
+    this.attackPower = parseInt(attackPower);
+    this.counterAttackPower = parseInt(counterAttackPower);
     this.image = image;
   }
 
@@ -27,3 +27,25 @@ class Character {
     return this.image;
   }
 }
+
+const imgPath = './assets/images/';
+const yourCharHolder = $('#your-character .character-holder');
+const obiWan = new Character('Obi-Wan Kenobi', 120, 8, 25, `${imgPath}obi-wan.jpg`);
+const luke = new Character('Luke Skywalker', 100, 6, 25, `${imgPath}luke-skywalker.jpg`);
+const maul = new Character('Darth Maul', 180, 10, 25, `${imgPath}darth-maul.jpg`);
+const sidious = new Character('Darth Sidious', 150, 4, 25, `${imgPath}darth-sidious.jpg`);
+const charactersArray = [obiWan, luke, maul, sidious];
+
+function createCharPanels() {
+  let content = '';
+  charactersArray.forEach(function (char, index) {
+    content += `<div id="char${index + 1}" class="char-panel">
+      <p>${char.myName}</p>
+      <img src="${char.myImage}" alt="${char.myName}">
+      <p>${char.myHealthPoints}</p>
+      </div>`;
+  });
+  yourCharHolder.html(content);
+}
+
+createCharPanels();
